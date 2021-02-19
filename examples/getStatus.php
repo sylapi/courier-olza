12a279f7814b92981f97e47731716531259cbc24
Daniel Serafinski <d.serafinski@sylapi.com>
2021-02-19 10:04:23 +0100

    examples

---
+++ examples/getStatus.php
@@ -0,0 +1,28 @@
+<?php
+use Sylapi\Courier\CourierFactory;
+
+$courier = CourierFactory::create('Olza',[
+    'login' => 'mylogin',
+    'password' => 'mypassword',
+    'sandbox' => true,
+    'requestLanguage' => 'pl',
+    'labelType' => 'A4',
+    'speditionCode' => 'GLS',
+    'shipmentType' => 'WAREHOUSE'
+]);
+
+/**
+ * GetStatus
+ */
+
+try {
+    $response = $courier->getStatus('123456');
+    if($response->hasErrors()) {
+        var_dump($response->getFirstError()->getMessage());
+    } else {
+        var_dump((string) $response); 
+    }
+} catch (\Exception $e) {
+    var_dump($e->getMessage());
+}
+    
